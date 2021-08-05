9293cd94e66f04605fcaebd36c07260589308c67
bigbany <bigbaby9728@naver.com>
2021-08-05 16:46:45 +0900

    'ch_01'

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # JavaSript Level Up
+
+## Ch 1. js 데이터
+---
+### 01. 문자
---
+++ main.js
@@ -0,0 +1,19 @@
+// JS 데이터
+
+// String: "" , '' ,``
+// Number
+// Boolean: true, false
+// undefined
+// null 
+// Array: []
+// Object: {}
+
+console.log(
+
+  'Hello world!'.indexOf('world')
+)
+
+const result ='Hello world!'.indexOf('hero')
+console.log(result)
+
+// String.prototype.indexOf()
